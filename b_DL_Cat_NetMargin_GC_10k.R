db01b8209b9178edd77e80d5973dc63d985a7dc3
Yeriko Vargas <ygvargass@icloud.com>
2020-01-14 11:57:39 -0500

    Building Neural Net; R-Keras process description
    Usage of R-Keras to construct DL models. Define architecture of model, compiling model & plotting loss and accuracy for all the epochs(line:29-45). Get predictions of the testing data (line:59). Evaluate accuracies of the test data set and training data set (line:63-76)

--- b_DL_Cat_NetMargin_GC_10k.R
+++ b_DL_Cat_NetMargin_GC_10k.R
@@ -72,98 +72,12 @@ head(y_data_real_oneh)
 #Accuracy based on data set that was trained with
 evaluate(model, x_data, y_data_oneh, verbose = 0)
 
-#Accuracy based on 10% of the data, sat approx 25 000 cars for evaluating accuracy & loss
+#Accuracy based on 25% of the data, sat approx 25 000 cars for evaluating accuracy & loss
 evaluate(model, x_data_test, y_data_real_oneh, verbose = 0)
 
 
-#+++++++++++++++++++++++++++++++**********************+++++++++++++++++++++++++++++++++++
-#library(reticulate)
-#use_condaenv(py36,conda="auto",required = FALSE)
-#++++++++++++++++++++++++++++++++**********************+++++++++++++++++++++++++++++++++++
-
-##Results Representation are the table of missclasification
-#if predicted column is the same keep.
-
-predxtst <- y_data_pred
-actuallvls <- y_test
-df <- data.frame(predxtst, actuallvls)
-head(df)
-
-#To analize bring predictors from xtest back to data set test_GC
-#then if prediction matches the actual label set a "good" column for data
-#all 1s representing entries that were correctly classified.
-test_GC$pred <-df$predxtst
-test_GC$good = ifelse(df$predxtst == df$d.nm, 1, 0)
-
-table(test_GC$good)
-
-#now split correct classified entries from test data and missclassified into two DS and chopp
-#off test_GC such that just 108 classificaion are included there
-test_GC_0 <- filter(test_GC[ -c(1:40)], good == 0)
-test_GC_1 <- filter(test_GC[ -c(1:40)], good == 1)
-
-#Dimension of data
-dim(test_GC_0)
-dim(test_GC_1)
-
-#cases where prediction and label are the same for testing data
-##Change entries from factors to actual numbers to sum later on
-test_GC_1[] <- lapply(test_GC_1, function(x) {
-  if(is.factor(x)) as.numeric(as.character(x)) else x
-})
-sapply(test_GC_1, class)
-##Cases where prediction and actual labels are different.
-test_GC_0[] <- lapply(test_GC_0, function(x) {
-  if(is.factor(x)) as.numeric(as.character(x)) else x
-})
-sapply(test_GC_0, class)
-
-#from test_GC_1 we can see all the right classifications and we can see in terms of
-#configurations which are prompting the different intervals of net margin
-
-result_T <- aggregate(test_GC_1[-c(109:112)] , 
-                              by=list(diach=test_GC_1$d.nm), 
-                              FUN=sum )
-result_F <- aggregate(test_GC_0[-c(109:112)] , 
-                      by=list(diach=test_GC_0$d.nm), 
-                      FUN=sum )
-
-#dt2 <- merge(result_F,result_T, by = "diach")
-#Reshape data
-result_T <- melt(result_T, id=c("diach"))
-result_F <- melt(result_F, id =c("diach"))
-#new data set with 4 columns representing  0 1 (correct classification) & 0 1 (missclasification)
-df1 <- spread(result_T, diach, value  )
-df2 <- spread(result_F, diach, value )
-dd = cbind(df1, df2)
-
-
-names(dd)[2] <- "under_correct"
-names(dd)[3] <- "over_correct"
-names(dd)[5] <- "under_missc"
-names(dd)[6] <- "over_missc"
-names(dd)
-
-Pcodes <- dd[c(2:6)]
-
-row.names(Pcodes) <- Pcodes$variable
-
-##first
-Pcodes$denum <-Pcodes$under_correct +Pcodes$over_correct +Pcodes$under_missc +Pcodes$over_missc
-Pcodes$num <-Pcodes$under_correct +Pcodes$over_correct
-#"is not accuracy now is going to be prob of under"
-Pcodes$probunder <- Pcodes$under_correct/(Pcodes$under_correct +Pcodes$under_missc )
-Pcodes$probover <- Pcodes$over_correct/(Pcodes$over_correct + Pcodes$over_missc)
-Pcodes$falsepositive <- Pcodes$under_missc/Pcodes$under_correct
-#Conditional Under given Over
-Pcodes$conditionalUnder <- Pcodes$falsepositive*Pcodes$probunder/Pcodes$probover
-names(Pcodes)
-
-#export csv file; prob codes missclassified and classified with Accuracies 
-
-#write.table(Pcodes, file = "/Users/yerikovargas/Documents/FCA/Pcodes_10k.csv",
-#            sep = ",", row.names=FALSE)
 
 ########################end---------------------------------------**********************************
 ########################end---------------------------------------**********************************
+
 
